93a7321bf72af0537b3142a10e7147413e4e1dbc
Liam_Araya <liamwork999@gmail.com>
2026-06-21 16:22:58 +0300

    Finished first version of (Daily_Expense_Tracker)

--- daily_expense_tracker.js
+++ daily_expense_tracker.js
@@ -9,3 +9,32 @@ A small JavaScript project that calculates:
 
 This project is part of my learning journey to practice real-world problem solving with JavaScript.
 */
+
+const expenses = [12, 35, 8, 50, 20, 15];
+const limit = 20;
+
+function totalAmountSpent(expenseList) {
+  return expenseList.reduce((sum, value) => sum + value, 0);
+}
+
+function largestExpense(expenseList) {
+  return Math.max(...expenseList);
+}
+
+function expensesAboveLimit(limitValue, expenseList) {
+  const aboveLimitExpenses = expenseList.filter((value) => value > limitValue);
+
+  return `Expenses above limit: ${aboveLimitExpenses.length} -> (${aboveLimitExpenses.join(
+    " and ",
+  )}) | Limit: ${limitValue}`;
+}
+
+function average(expenseList) {
+  return totalAmountSpent(expenseList) / expenseList.length;
+}
+
+console.log("\nExpenses Display\n");
+console.log(`💠 Total: ${totalAmountSpent(expenses)}`);
+console.log(`💠 Largest expense: ${largestExpense(expenses)}`);
+console.log(`💠 ${expensesAboveLimit(limit, expenses)}`);
+console.log(`💠 Average: ${average(expenses)}`);
